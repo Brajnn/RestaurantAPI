de3406f5c03e429c115a0037ef82912946d08263
Brajnn <olido22@wp.pl>
2023-11-14 11:41:24 +0100

    Dodanie walidacji do modelu CreateRestaurantDto

--- RestaurantAPI/Models/CreateRestaurantDto.cs
+++ RestaurantAPI/Models/CreateRestaurantDto.cs
@@ -1,15 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+using System.ComponentModel.DataAnnotations;
 
 namespace RestaurantAPI.Models
 {
     public class CreateRestaurantDto
     {
+        [Required]
+        [MaxLength(25)]
         public string Name { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
         public bool HasDelivery { get; set; }
         public string City { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Street { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string PostalCode { get; set; }
         public string ContactEmail { get; set; }
         public string ContactNumber { get; set; }
