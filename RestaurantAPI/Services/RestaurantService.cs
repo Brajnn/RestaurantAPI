42b1e88a0ebcd2ec15fc4b2bab34aac5b3a15f70
Brajnn <olido22@wp.pl>
2023-11-14 20:09:59 +0100

    dodanie paczki nLog i stworzenie pierwszego loga

--- RestaurantAPI/Services/RestaurantService.cs
+++ RestaurantAPI/Services/RestaurantService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using RestaurantAPI.Entities;
 using RestaurantAPI.Models;
 using System.Collections.Generic;
@@ -11,14 +12,17 @@ namespace RestaurantAPI.Services
     {
         private readonly RestaurantDbContext _dbContext;
         private readonly IMapper _mapper;
+        private readonly ILogger _logger;
 
-        public RestaurantService(RestaurantDbContext dbContext, IMapper mapepr)
+        public RestaurantService(RestaurantDbContext dbContext, IMapper mapepr, ILogger<RestaurantService> logger)
         {
             _dbContext = dbContext;
             _mapper = mapepr;
+            _logger = logger;
         }
         public bool Delete(int id)
         {
+            _logger.LogError($"Restaurant with id:  {id} DELETE action invoked");
             var restaurant = _dbContext
             .Restaurants
             .FirstOrDefault(r => r.Id == id);
